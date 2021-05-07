e3ab247efc4e3da8556d1a201811163cb1209482
Krishi Sharma <krishi@Krishis-MacBook-Pro.local>
2021-05-07 17:36:13 -0500

    remove unnecessary imports and add recipes

--- examples/1_connect_to_db/main.py
+++ examples/1_connect_to_db/main.py
@@ -5,7 +5,7 @@ from kfai_env.environment.register import Environment
 from kfai_sql_chemistry.db.database_config import DatabaseConfig
 
 # Setup this object ahead of time
-from kfai_sql_chemistry.db.main import setup_database_engines, register_databases
+from kfai_sql_chemistry.db.main import register_databases
 from kfai_sql_chemistry.db.session import AppSession
 
 e = Environment('./env')
@@ -18,7 +18,6 @@ database_map: Dict[str, DatabaseConfig] = {
 if __name__ == "__main__":
     # Call Register at the beginning of your application
     register_databases(database_map)
-    setup_database_engines()
 
     with AppSession("main") as session:
         print(session.execute("SELECT 1").fetchall())
