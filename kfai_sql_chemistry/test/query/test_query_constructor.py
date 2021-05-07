4cd0b9be7dc95485d52f044273d5c0e00612e23e
Endurance Idehen <endurance.idehen@kungfu.ai>
2021-05-07 09:02:04 -0500

    a little bit of refactor so we can run tests!

--- kfai_sql_chemistry/test/query/test_query_constructor.py
+++ kfai_sql_chemistry/test/query/test_query_constructor.py
@@ -8,45 +8,33 @@ from moto import mock_secretsmanager
 
 from kfai_sql_chemistry.aws.aws_db_config import AwsDbConfig
 from kfai_sql_chemistry.db.database_config import DatabaseConfig
+from kfai_sql_chemistry.db.main import engines, register_databases
+from kfai_sql_chemistry.test.query.models import BaseDbModel
 from kfai_sql_chemistry.utils.setup_for_testing import setup_db_for_tests
 
 
 class QueryConstructorTest(unittest.TestCase):
 
     def setUp(self):
-        os.environ['ENV'] = 'AWS-TEST'
+        os.environ['ENV'] = 'TEST'
         e = Environment('./kfai_sql_chemistry/test/env')
-        e.register_environment("AWS-TEST")
+        e.register_environment("TEST")
         e.load_env()
+
+        database_map: Dict[str, DatabaseConfig] = {
+            "main": DatabaseConfig.from_local_env("main")
+        }
+
+        register_databases(database_map)
+
+        engine = engines.get_engine("main")
+        setup_db_for_tests(engine, BaseDbModel.metadata)
 
     def tearDown(self):
         os.environ['ENV'] = ''
 
-
-    # start the database session as session here
-
-    VehicleTestDbModel = declarative_base()
-
-    class CarModel(VehicleTestDbModel):
-        __tablename__ = "Car"
-
-        car_id = Column("CarId", Integer, primary_key=True, autoincrement=True)
-        car_make = Column("CarMake", String(100))
-        car_model = Column("CarModel", String(100))
-        car_year = Column("CarYear", Integer)
-        car_type = Column("CarType", String(25))
-
-    class DriverModel(VehicleTestDbModel):
-        __tablename__ = "Driver"
-
-        driver_id = Column("DriverId", Integer, primary_key=True, autoincrement=True)
-        driver_first_name = Column("DriverFirstName", String(100))
-        driver_last_name = Column("DriverLastName", String(100))
-        driver_state = Column("DriverState", String(2))
-
-    test = engines.get_engine("test_db")
-    setup_db_for_tests(test, VehicleTestDbModel.metadata)
-
-    # figure out how to handle inputs
-    QueryConstructor.join(session)
+    def test_something(self):
+        pass
+        # figure out how to handle inputs
+        # QueryConstructor.join(session)
 
